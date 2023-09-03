d555894855e8ec22aa62ec184596c05457c3c89f
Adrian Enis <adrian2000.alfonso.@gmail.com>
2023-09-02 22:52:22 -0400

    Añadir captura de datos de formulario a userData

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
-  const data = Object.fromEntries(new FormData(e.target))
-  // alert(JSON.stringify(data))
-  console.log(data)
+  
+  const email = document.querySelector("#email").value;
+  const pass = document.querySelector("#pass").value;
+
+  console.log(email)
+  console.log(pass)
+
+  //almacenar data
+  const userData = {
+    email: email,
+    pass: pass,
+  }
+
+  console.log(userData)
+
+  const emailRegex = /@gmail\.com$/;
+  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+  if(!emailRegex.test(email)){
+    alert("El correo debe tener la extension @gmail.com")
+  }
+  if(!passRegex.test(pass)){
+    alert("La contraseña debe tener como minimo 8 caracteres y 1 mayuscula, ademas de un signo especial")
+  }
 })
